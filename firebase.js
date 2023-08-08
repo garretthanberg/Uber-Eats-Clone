db987644e1772515610d5b7b8738125eafb3cec7
Garrett Hanberg <115447682+garretthanberg@users.noreply.github.com>
2023-08-07 19:26:25 -0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase/compat";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBj40JNljR11ZJfM1l-pnVu2QFWncM1TIc",
-  authDomain: "rn-uber-eats-clone-gh.firebaseapp.com",
-  projectId: "rn-uber-eats-clone-gh",
-  storageBucket: "rn-uber-eats-clone-gh.appspot.com",
-  messagingSenderId: "909470529779",
-  appId: "1:909470529779:web:9b7fd2276f861203e61f86",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
